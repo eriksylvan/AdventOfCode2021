27e3dd8b3a2780c4b685275cc4acaffcbe2a1af4
Erik Sylvan <erik.sylvan@delaval.com>
2021-11-17 16:30:39 +0100

    start up

---
+++ day_01.py
@@ -0,0 +1,38 @@
+# https://adventofcode.com/2021/day/1
+
+
+
+inputFile = 'input/01_input'
+
+
+def dummy(d):
+    '''Dummy function for test Doubles the input
+    Parameters:
+    d (int): the input 
+    Returns:
+    int: the duble input
+    '''
+    return d*2
+
+
+def day01PartOne():
+    input = 1
+    output = dummy(input)
+
+    print(
+        f'Solution Day 01, Part one:\nAnswer: {output} \n\n')
+
+
+def day01PartTwo():
+    
+    print(
+        f'Solution Day 01, Part one:\nToBeDone \n\n')
+
+
+if __name__ == "__main__":
+    day01PartOne()
+    day01PartTwo()
+
+
+# Run from terminal:
+# $ python day_01.py
---
+++ test/test_day_01.py
@@ -0,0 +1,14 @@
+import unittest
+import day_01
+
+class TestDay01(unittest.TestCase):
+    
+    def test_fuel_calculation(self):
+        self.assertEqual(day_01.dummy(1), 2)
+        
+if __name__ == '__main__':
+    unittest.main()
+
+
+# Run tests from terminal:
+# $ python -m unittest day_01_test.py 
