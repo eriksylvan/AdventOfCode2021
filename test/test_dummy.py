20238223143aa855eaeaa012fb19b84a8015b106
Erik Sylvan <erik.sylvan@delaval.com>
2021-11-30 20:27:02 +0100

    fix

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -10,6 +10,7 @@ def test_dummy_02():
 @pytest.mark.parametrize("test_input, expected_result", [([10,10,10],30), ([10,20,30],60), ([10,-10,10],10), ([1,1,1,1,1,1,-8],-2)])
 def test_dummy_03(test_input, expected_result):
     assert dummy_day.summy(test_input) == expected_result
+    
 
 # Run tests from terminal:
 # $ pytest test/test_dummy.py 
