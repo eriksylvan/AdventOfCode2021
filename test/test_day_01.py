92eecbb86f721bc6c94e8134dbd436b887d53025
Erik Sylvan <erik.sylvan@delaval.com>
2021-11-30 18:41:39 +0100

    PyTest

--- test/test_day_01.py
+++ test/test_day_01.py
@@ -1,14 +1,8 @@
-import unittest
 import day_01
-
-class TestDay01(unittest.TestCase):
     
-    def test_day01(self):
-        self.assertEqual(day_01.dummy(1), 2)
+def test_day01():
+    assert day_01.dummy(1) == 2
         
-if __name__ == '__main__':
-    unittest.main()
-
 
 # Run tests from terminal:
-# $ python -m unittest day_01_test.py 
+# $ pytest test/test_day_01.py 
