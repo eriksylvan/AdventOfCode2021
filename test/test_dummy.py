d13247c91edc6913ee5d03782a346fac03ac3678
Erik Sylvan <erik.sylvan@delaval.com>
2021-12-01 18:56:17 +0100

    Solution day 1

--- test/test_dummy.py
+++ test/test_dummy.py
@@ -7,6 +7,7 @@ def test_dummy_01():
 def test_dummy_02():
     assert dummy_day.summy([1,2,3]) == 6
 
+@pytest.mark.skip(reason="dummy")
 @pytest.mark.parametrize("test_input, expected_result", [([10,10,10],30), ([10,20,30],60), ([10,-10,10],10), ([1,1,1,1,1,1,-8],-2)])
 def test_dummy_03(test_input, expected_result):
     assert dummy_day.summy(test_input) == expected_result
