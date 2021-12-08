560d6230e91726e814d2c8dbe7bc160d44c6d54d
Erik Sylvan <erik.sylvan@delaval.com>
2021-12-08 23:58:55 +0100

    Solution Day 8 Part 2

--- day_08.py
+++ day_08.py
@@ -1,12 +1,12 @@
 # https://adventofcode.com/2021/day/8
-import parse
+import itertools
 
 
 inputFile = 'input/08_input'
 
 
 def file2List(file):
-    '''Reads file with vectors on format ex '8,0 -> 0,8' returs list of lines
+    '''Reads file, returs list of lines
     Parameters:
     file: the input file
     Returns:
@@ -20,10 +20,9 @@ def file2List(file):
         
     return list
 
-def readVect(inp):
-    '''Reads list with vectors on format ex '8,0 -> 0,8' returs list with lists [8,0,0,8]
+def readDigitData(inp):
+    '''Reads list data abour 7 segments digitis, splits it and return two lists, output and input 
     '''
-    vectList = []
     inputList = []
     outputList = []
      
@@ -34,33 +33,133 @@ def readVect(inp):
     return inputList, outputList
 
 def countEasy(l):
+    '''
+    Counts the digits that are easy to rekognize (Part1)
+    '''
     count = 0;
     for r in l:
         for i in r:
             if len(i) in (2,3,4,7):
                 count +=1
     return count
+
+
+def findWieringPattern(digits):
+    '''
+    This method finfd the wiering by trying out all possible wiering. 
+    When first correct wiering is found it stops and returns the wiering pattern as a tuple.
+    Position of the letter in the wiering pattern corresponds to one segment on the 7 segment display as follow:
     
+     6666
+    5    1
+    5    1
+     ffff
+    4    2
+    4    2
+     3333
+    
+    For example: the wiering below is represented as: ('d', 'a', 'b', 'c', 'g', 'e', 'f')
+    
+     dddd
+    e    a
+    e    a
+     ffff
+    g    b
+    g    b
+     cccc
+    '''
+    segments = 'abcdefg'
+    per = list(itertools.permutations(segments))
+    
+    
+    for pattern in per:
+        allOK = True
+        for dig in digits:
+            d = getDigit(dig,pattern)
+            if d == -1:
+                allOK = False
+                break
+        if allOK:
+            #correct wiering found
+            return pattern
+    return False
+
+        
+def getDigit(dig,pattern):
+    '''
+    Return the digit represented by the segments in the input with given the wiering pattern
+    Parameters:
+    dig: the activated wires giving the digit as string (a-f)
+    pattern: wiering pattern as a tuple ('a', 'b', 'c', 'd', 'e', 'f', 'g')
+    Returns:
+    digit: number on display as int, -1 if unvalid number
+    '''
+    digList = set(dig)
+    if digList == {pattern[0], pattern[1], pattern[2], pattern[3], pattern[4], pattern[5]}:
+        return 0
+    elif digList == {pattern[1], pattern[2]}:
+        return 1
+    elif digList == {pattern[0], pattern[1], pattern[3], pattern[4], pattern[6]}:
+        return 2
+    elif digList == {pattern[0], pattern[1], pattern[2], pattern[3], pattern[6]}:
+        return 3
+    elif digList == {pattern[1], pattern[2], pattern[5], pattern[6]}:
+        return 4
+    elif digList == {pattern[0], pattern[2], pattern[3], pattern[5], pattern[6]}:
+        return 5
+    elif digList == {pattern[0], pattern[2], pattern[3], pattern[4], pattern[5], pattern[6]}:
+        return 6
+    elif digList == {pattern[0], pattern[1], pattern[2]}:
+        return 7
+    elif digList == {pattern[0], pattern[1], pattern[2], pattern[3], pattern[4], pattern[5], pattern[6]}:
+        return 8
+    elif digList == {pattern[0], pattern[1], pattern[2], pattern[3], pattern[5], pattern[6]}:
+        return 9
+    else: 
+        return -1
+    
+
+def decodeOutputValue(inp, out):
+    '''
+    Finds the wiering pattern, decodes and return the output value 
+    '''
+    p = findWieringPattern(inp)
+    o1 = getDigit(out[0], p)    
+    o2 = getDigit(out[1], p)
+    o3 = getDigit(out[2], p)
+    o4 = getDigit(out[3], p)        
+    return (o1*1000+o2*100+o3*10+o4)
+    
+
                 
 def day08PartOne():
     input = file2List(inputFile)
-    i,o = readVect(input)
+    i,o = readDigitData(input)
     output = countEasy(o)
     print(f'# Solution Day 08, Part one:\n# Answer: {output} \n\n')
 
 
 def day08PartTwo():
     input = file2List(inputFile)
-    
-    output = "WIP"
+    inp,out = readDigitData(input)
+    values = []
+    for i in range(len(input)):
+        values.append(decodeOutputValue(inp[i],out[i]))
+    output = sum(values)
     print(f'# Solution Day 08, Part two:\n# Answer: {output} \n\n')
 
 
 
 if __name__ == "__main__":
     day08PartOne()
-    #day08PartTwo()
+    day08PartTwo()
 
+# Solution Day 08, Part one:
+# Answer: 237 
+
+
+# Solution Day 08, Part two:
+# Answer: 1009098 
 
 # Run from terminal:
 # $ python day_08.py
--- test/test_day_08.py
+++ test/test_day_08.py
@@ -7,16 +7,44 @@ inputFile = "input/test_08_input"
 
 def test_input_file():
     l = day_08.file2List(inputFile)
-    inputList, outputList = day_08.readVect(l)
+    inputList, outputList = day_08.readDigitData(l)
     assert len(inputList) == 10
     assert inputList[0][0] == 'be'
     assert outputList[9][3] == 'bagce'
 
 def test_countEasy():
     l = day_08.file2List(inputFile)
-    inputList, outputList = day_08.readVect(l)
+    inputList, outputList = day_08.readDigitData(l)
     assert day_08.countEasy(outputList) == 26
-    
 
+def test_isDigit_3():
+    d = day_08.getDigit('abcdg', ('a', 'b', 'c', 'd', 'e', 'f', 'g'))
+    assert d == 3
+
+@pytest.mark.parametrize("test_input, test_output", [('acedgfb',8), ('cdfbe',5), ('gcdfa',2), ('fbcad',3), ('dab',7), ('cefabd',9), ('cdfgeb',6), ('eafb',4), ('cagedb',0), ('ab',1)])
+def test_getDigit(test_input, test_output):
+    d = day_08.getDigit(test_input, ('d', 'a', 'b', 'c', 'g', 'e', 'f'))
+    assert d == test_output
+
+def test_getDigit_Unvalid():
+    d = day_08.getDigit('adefg', ('a', 'b', 'c', 'd', 'e', 'f', 'g'))
+    assert d == -1
+
+def test_findWieringPattern():
+    digits = ['acedgfb', 'cdfbe', 'gcdfa', 'fbcad', 'dab', 'cefabd', 'cdfgeb', 'eafb', 'cagedb', 'ab']
+    d = day_08.findWieringPattern(digits)
+    assert ''.join(d) == 'dabcgef'
+
+def test_getSumFromExample():
+    input = day_08.file2List(inputFile)
+    inp,out = day_08.readDigitData(input)
+    values = []
+    for i in range(len(input)):
+        values.append(day_08.decodeOutputValue(inp[i],out[i]))
+    output = sum(values)
+    assert output == 61229
+    
 # Run tests from terminal:
 # $ pytest test/test_day_08.py 
+
+# , , , ]
