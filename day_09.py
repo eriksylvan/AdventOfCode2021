d5f2843fc9d039661c37c4fe35a8468d4e0cf8fc
Erik Sylvan <erik.sylvan@delaval.com>
2021-12-10 01:43:16 +0100

    Solution Day 9

--- day_09.py
+++ day_09.py
@@ -1,5 +1,7 @@
 # https://adventofcode.com/2021/day/9
 
+import math
+
 
 inputFile = 'input/09_input'
 
@@ -19,19 +21,75 @@ def file2List(file):
         
     return list
 
-def riskLevel(heatmap):
-    return 0
+def findLocalMin(heatmap):
+    mx= len(heatmap[0])
+    my= len(heatmap)
+    localMin = {}
+    extendedHeatmap = extendHeatmap(heatmap)
     
+    for y in range(1,my+1):
+        for  x in range(1,mx+1):
+            n = int(extendedHeatmap[y-1][x])
+            s = int(extendedHeatmap[y+1][x])
+            w = int(extendedHeatmap[y][x-1])
+            e = int(extendedHeatmap[y][x+1])
+            c = int(extendedHeatmap[y][x])
+            if (c<n)and(c<s)and(c<w)and(c<e):
+                # local min found
+                localMin[(x-1,y-1)] = c
+    return localMin
+
+def extendHeatmap(heatmap):
+    extendedHeatmap = ['9'*(len(heatmap[0])+2)]
+    for row in heatmap:
+        extendedHeatmap.append('9'+row+'9')
+    extendedHeatmap.append('9'*(len(heatmap[0])+2))
+    return extendedHeatmap
+
+def find3LargestBasinSize(heatmap):
+    extendedHeatmap = extendHeatmap(heatmap)
+    
+    localMin = findLocalMin(heatmap)
+    sizes = []
+    for x,y in localMin.keys():
+        visited = []
+        sizes.append(basinSize(x+1,y+1,0,visited,extendedHeatmap))
+
+    sizes.sort(reverse=True)
+    return sizes[0:3]
+
+
+def basinSize(x,y, size,visited,heatmap):
+    if (x,y) in visited: 
+        return 0     
+    visited.append((x,y))
+    if heatmap[y][x]=='9':
+        return 0
+    else:
+        size=1
+    # N
+    size += basinSize(x,y-1, size, visited,heatmap)
+    # E
+    size += basinSize(x+1,y, size, visited,heatmap)
+    # S
+    size += basinSize(x,y+1, size, visited,heatmap)
+    # W
+    size += basinSize(x-1,y, size, visited,heatmap)
+    return size
                 
 def day09PartOne():
     input = file2List(inputFile)
-    output = "WIP"
+    localMin = findLocalMin(input)
+    output = sum(localMin.values()) + len(localMin)
+           
     print(f'# Solution Day 09, Part one:\n# Answer: {output} \n\n')
 
 
 def day09PartTwo():
     input = file2List(inputFile)
-    output = "WIP"
+    localMin = findLocalMin(input)
+    o3 = find3LargestBasinSize(input)
+    output = math.prod(o3)
     print(f'# Solution Day 09, Part two:\n# Answer: {output} \n\n')
 
 
@@ -40,6 +98,12 @@ if __name__ == "__main__":
     day09PartOne()
     day09PartTwo()
 
+# Solution Day 09, Part one:
+# Answer: 518 
+
+
+# Solution Day 09, Part two:
+# Answer: 949905 
 
 # Run from terminal:
 # $ python day_09.py
--- test/test_day_09.py
+++ test/test_day_09.py
@@ -8,12 +8,38 @@ heatmap = ["2199943210",
 "8767896789",
 "9899965678"]
 
+extendedHeatmap = ["999999999999",
+"921999432109",
+"939878949219",
+"998567898929",
+"987678967899",
+"998999656789",
+"999999999999"]
+
+def test_extendHeatMap():
+    assert day_09.extendHeatmap(heatmap) == extendedHeatmap
 
 def test_riskLevel():
-    output = day_09.riskLevel(heatmap)
+    localMin = day_09.findLocalMin(heatmap)
+    output = sum(localMin.values()) + len(localMin)
     assert output == 15
+
+def test_findBasinSize():
+    exhm = day_09.extendHeatmap(heatmap)
+    visited=[]
+    output = day_09.basinSize(2,1,0,visited,exhm)
+    assert output == 3
+
+def test_findBasinSize2():
+    exhm = day_09.extendHeatmap(heatmap)
+    visited=[]
+    output = day_09.basinSize(10,1,0,visited,exhm)
+    assert output == 9
+
+def test_find3LargestBasinSize():
+    localMin = day_09.findLocalMin(heatmap)
+    output = day_09.find3LargestBasinSize(heatmap)
+    assert output == [14, 9, 9]
     
-
-
 # Run tests from terminal:
 # $ pytest test/test_day_08.py 
